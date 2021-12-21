c28d36715e79c76893b8bb33da1a92e22e24c243
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-12-22 02:06:29 +0530

    redux starting again

--- Web development 2021/MERN/post/client/src/api/index.js
+++ Web development 2021/MERN/post/client/src/api/index.js
@@ -3,4 +3,3 @@ import axios from "axios";
 const url = "http://localhost:5500/posts/";
 
 export const featchPost = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
--- Web development 2021/MERN/post/client/src/index.js
+++ Web development 2021/MERN/post/client/src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reducers from "./reducers";
 
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
